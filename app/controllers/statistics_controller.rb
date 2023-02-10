66b2e0d53c3ec033254868e65ade5ec3b2c16fc0
viktorJMarton <martonviktor666@gmail.com>
2023-02-10 14:38:43 +0000

    styling fixation

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,12 +1,7 @@
 class StatisticsController < ApplicationController
   def show
-    @most_popular_house =House.left_joins(:bookings).group(:id).order('count(bookings.id) DESC').first
+    @most_popular_house = House.left_joins(:bookings).group(:id).order('count(bookings.id) DESC').first
 
-
-    @most_successful_year =Booking.group("strftime('%Y', day)").order("count(day) DESC").limit(1).count.keys.first
-
-
+    @most_successful_year = Booking.group("strftime('%Y', day)").order('count(day) DESC').limit(1).count.keys.first
   end
-
-
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,6 @@ class House < ApplicationRecord
   has_many :bookings
 
   validates :name, presence: true, uniqueness: true
-  
+
   scope :ordered, -> { order(:name, :city) }
 end
